237bf49021e50710392bc3cd9d8a333ba180aa0c
Suzy Burgum <suzyburgum@gmail.com>
2025-03-25 15:13:50 -0400

    Integrate Sentry for error tracking and performance monitoring

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,27 @@
-const nextConfig = {};
 
-module.exports = nextConfig;
+
+import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
+
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+};
+
+const sentryWebpackPluginOptions = {
+  org: "common-block-studios",
+  project: "javascript-nextjs",
+
+  silent: !process.env.CI, // Only log during CI builds
+
+  widenClientFileUpload: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+
+ 
+};
+
+
+export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
