8c03bc43c537ba8e364c93e9e740c4ecfae1b0a9
Suzy Burgum <suzyburgum@gmail.com>
2025-03-25 18:42:00 -0400

    Refactor Footer and FloatingNav components for improved layout and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,7 @@ import { socialMedia } from "@/data";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 mb-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-100"
-        />
-      </div>
+    <footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
@@ -29,8 +22,8 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col items-center md:justify-around justify-between">
-        <p className="md:text-base text-sm md:font-normal font-light">
+      <div className="flex mt-16 md:flex-row flex-col items-center sm:justify-around justify-between">
+        <p className="md:text-base text-sm md:font-normal font-light sm: mb-5">
           Copyright © 2025 Suzy{" "}
         </p>
         <div className="flex items-center md:gap-3 gap-6">
